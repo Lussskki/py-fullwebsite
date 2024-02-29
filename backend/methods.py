f29f663cd34a8dd714aa9badb3610b5ec23a2ce3
Luka Guledani <lukaguledani123@gmail.com>
2024-03-01 00:06:13 +0400

    added get method for testing in postman

--- backend/methods.py
+++ backend/methods.py
@@ -2,12 +2,32 @@ from bson import ObjectId
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from database import db_connection
+import os
 
 router = APIRouter()
 
 class Item(BaseModel):
     name: str  
     last: str
+
+# get method only for testing in postman yet
+@router.get("/")
+async def get_item():
+    try:
+        db = await db_connection()
+        if db is None:
+            raise HTTPException(status_code=500, detail="Failed to connect to the database")
+        data_list = list(db.test.find())
+        for data in data_list:
+            data['_id'] = str(data['_id'])
+        return data_list
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to retrieve data") from e
+    finally:
+        if db is not None:
+            db.client.close()
+
+
 
 @router.post("/item/")
 async def post_item(item: Item):
@@ -31,7 +51,7 @@ async def delete_item(item_id: str):
         if db is None:
             raise HTTPException(status_code=500, detail="Failed to connect to database")
         item_id_obj = ObjectId(item_id)
-        result = db.test.delete_one({"_id": item_id_obj})  # Use the ObjectId
+        result = db.test.delete_one({"_id": item_id_obj})  
         if result.deleted_count == 1:
             return {"message": "Item deleted successfully"}
         else:

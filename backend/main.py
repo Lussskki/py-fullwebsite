b214a299cff256c4f4e147dc5b2209f63fb086ac
Luka Guledani <lukaguledani123@gmail.com>
2024-03-03 03:32:00 +0400

    added style.css

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from methods import router
 from database import db_connection
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
+from fastapi.responses import FileResponse  
 import os
 
 load_dotenv()
@@ -18,11 +19,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
 @app.get("/", response_class=HTMLResponse)
 async def serve_index():
     with open(os.path.join("frontend", "index.html"), "r") as f:
         index_html = f.read()
     return HTMLResponse(content=index_html, status_code=200)
+
+@app.get("/style.css")
+async def serve_css():
+    return FileResponse("frontend/style.css", media_type="text/css")
 
 app.include_router(router)
 
